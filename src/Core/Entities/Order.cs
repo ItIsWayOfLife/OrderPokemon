cdaf847b410b484d7b145a32ba0c00e8f659a61f
Anton Gunko <antongunko@outlook.com>
2021-04-22 21:01:31 +0300

    Add core layer and Entities

---
+++ src/Core/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime DateTimeOrder { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ src/Core/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
