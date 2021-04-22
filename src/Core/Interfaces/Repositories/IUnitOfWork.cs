263e5ce1f16eadb4b2ef643e1b2e8c7a3016b11e
Anton Gunko <antongunko@outlook.com>
2021-04-22 21:55:36 +0300

    Add UnitOfWork

---
+++ src/Core/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Core.Interfaces.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        /// Order repository. Working DB with entity Order.
+        /// </summary>
+        IOrderRepository Order { get; }
+
+        /// <summary>
+        /// User repository. Working DB with entity User.
+        /// </summary>
+        IUserRepository User { get; }
+
+        /// <summary>
+        /// Save data in DB.
+        /// </summary>
+        void Save();
+    }
+}
---
+++ src/Infrastructure/Repositories/EFUnitOfWork.cs
@@ -0,0 +1,68 @@
+﻿using Core.Interfaces.Repositories;
+using Infrastructure.Entities;
+using System;
+
+namespace Infrastructure.Repositories
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext _applicationContext;
+
+        private OrderRepository _orderRepository;
+        private UserRepository _userRepository;
+
+        public EFUnitOfWork(ApplicationContext applicationContext)
+        {
+            _applicationContext = applicationContext;
+        }
+
+        public IOrderRepository Order
+        {
+            get
+            {
+                if (_orderRepository == null)
+                {
+                    _orderRepository = new OrderRepository(_applicationContext);
+                }
+                return _orderRepository;
+            }
+        }
+
+        public IUserRepository User
+        {
+            get
+            {
+                if (_userRepository == null)
+                {
+                    _userRepository = new UserRepository(_applicationContext);
+                }
+                return _userRepository;
+            }
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _applicationContext.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Save()
+        {
+            _applicationContext.SaveChanges();
+        }
+    }
+}
